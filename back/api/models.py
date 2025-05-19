c43f160beab57ba3dfd5c7dd0b1e888b7f0778c6
Lucas Duarte Bareli <42220020894@>
2025-05-19 16:49:39 -0300

    19-05-2025

--- back/api/models.py
+++ back/api/models.py
@@ -4,14 +4,12 @@ class Sensores(models.Model):
     sensor = models.CharField(max_length=100)
     mac_adress = models.CharField(max_length=256)
     unidade_med = models.CharField(max_length=100)
-    valor = models.CharField(max_length=256)
     latitude = models.FloatField()
     longitude = models.FloatField()
-    status = models.CharField(max_length=100)
-    timestamp = models.CharField(max_length=100)
+    status = models.BooleanField()
 
 class Ambientes(models.Model):
-    sig = models.CharField(max_length=100)
+    sig = models.IntegerField()
     descricao = models.CharField(max_length=256)
     ni = models.CharField(max_length=100)
     responsavel = models.CharField(max_length=100)
@@ -19,4 +17,5 @@ class Ambientes(models.Model):
 class Historicos(models.Model):
     sensor = models.ForeignKey('Sensores', on_delete=models.CASCADE)
     ambiente = models.ForeignKey('Ambientes', on_delete=models.CASCADE)
-    observacoes = models.TextField()
+    Valor = models.FloatField()
+    timestamp = models.IntegerField()
