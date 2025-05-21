2d378ae76eb3a8ec73bed6dd68297389dc9665ea
Lucas Duarte Bareli <42220020894@>
2025-05-21 17:04:44 -0300

    21-05-2025

--- back/smartCity/settings.py
+++ back/smartCity/settings.py
@@ -62,6 +62,7 @@ SIMPLE_JWT = {
     }
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware', 
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -72,6 +73,11 @@ MIDDLEWARE = [
 ]
 
 CORS_ORIGIN_ALLOW_ALL = True
+
+CORS_ALLOWED_ORIGINS = [
+    'http://localhost:5173',  
+]
+
 
 CORS_ALLOW_METHODS = [
     'DELETE',
